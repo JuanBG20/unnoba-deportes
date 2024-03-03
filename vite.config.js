00ac3ceeea48a37f40c87751f21dd8af0472cac1
JuanBG20 <juanbgalvan.19@gmail.com>
2024-03-03 18:44:46 -0300

    Base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: 'https://juanbg20.github.io/unnoba-deportes'
+  plugins: [react()]
 })
